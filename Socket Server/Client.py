47f87ec1a17c204fee4b49fc428b0c32a411f795
Evoiis <athonly@gmail.com>
2018-10-16 13:37:24 -0700

    Working client/server Makes Pepper speak

--- Socket Server/Client.py
+++ Socket Server/Client.py
@@ -2,24 +2,28 @@ from socket import *
 import time
 import json
 
-#serverName = 'localhost'
+#set server IP and Port
 serverName = "10.0.0.3"
-serverPort = 9100
+serverPort = 9108
+#create socket
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
+#create JSON Packet
 integer = 15
 packet = '{"test": '+str(integer)+'}'
 print("Packet to send contains: data = "+str(integer))
-#print("Packet thing = "+ packet[0])
 
+#send packet to server
 clientSocket.sendto( packet.encode(), (serverName, serverPort))
 
+#wait for receiver to send a response
 receiverResponse, serverAddress = clientSocket.recvfrom(2048)
+
+#on response decode and parse Json
 decoded_packet = receiverResponse.decode()
 parsed_json = json.loads(receiverResponse)
 
 print(receiverResponse)
-#print (parsed_json)
-#print ("Packet received contains: data = " + str(parsed_json['test']))
 
+#close socket
 clientSocket.close()
