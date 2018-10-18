723ec47c585cce76e10badb22ced98c45baab932
Evoiis <athonly@gmail.com>
2018-10-18 14:56:13 -0700

    Pull from master and Cleaned up Files

---
+++ Socket Server/Client.py
@@ -0,0 +1,29 @@
+from socket import *
+import time
+import json
+
+#set server IP and Port
+#serverName = "localhost"
+serverName = "10.0.0.3"
+serverPort = 9050
+#create socket
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+#create JSON Packet
+packet = '{"msg": "Hello"}'
+print("Packet to send contains: data = hello")
+
+#send packet to server
+clientSocket.sendto( packet.encode(), (serverName, serverPort))
+
+#wait for receiver to send a response
+receiverResponse, serverAddress = clientSocket.recvfrom(2048)
+
+#on response decode and parse Json
+decoded_packet = receiverResponse.decode()
+parsed_json = json.loads(receiverResponse)
+
+print(receiverResponse)
+
+#close socket
+clientSocket.close()
