23836f49c6e5c9514199c22912f75feea3363fd8
subinb <subin_bae@sfu.ca>
2018-10-22 20:26:00 -0700

    Created Messenger User Interface
    - Added Author, Message models
    - Added ChatActivit
    - Added activity_chat.xml for layout

---
+++ Android/app/src/main/java/theshakers/cmpt276/sfu/ca/robottelepresense/Server/ChatActivity.java
@@ -0,0 +1,56 @@
+package theshakers.cmpt276.sfu.ca.robottelepresense.Server;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.stfalcon.chatkit.messages.MessageInput;
+import com.stfalcon.chatkit.messages.MessagesList;
+import com.stfalcon.chatkit.messages.MessagesListAdapter;
+
+import java.util.Date;
+
+import theshakers.cmpt276.sfu.ca.robottelepresense.Model.Author;
+import theshakers.cmpt276.sfu.ca.robottelepresense.Model.Message;
+import theshakers.cmpt276.sfu.ca.robottelepresense.R;
+
+/**
+ * Created by baesubin on 2018-10-22.
+ */
+
+public class ChatActivity extends AppCompatActivity {
+    MessageInput inputView = null;
+    MessagesList messagesList = null;
+    MessagesListAdapter<Message> adapter = null;
+    String senderId = "User";
+    //Boolean isSender = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        inputView = (MessageInput) findViewById (R.id.input);
+        messagesList = (MessagesList) findViewById (R.id.messagesList);
+
+        adapter = new MessagesListAdapter<>(senderId, null);
+        messagesList.setAdapter(adapter);
+
+        inputView.setInputListener(new MessageInput.InputListener() {
+            @Override
+            public boolean onSubmit(CharSequence input) {
+                addMsgToAdapter("User", input.toString());
+                return true;
+            }
+        });
+    }
+
+
+    private void addMsgToAdapter(String id, String inputText) {
+        Message message;
+        Author author = new Author(id, id, "null");
+        message = new Message(id, inputText, author, new Date());
+        adapter.addToStart(message, true);
+    }
+
+}
+
