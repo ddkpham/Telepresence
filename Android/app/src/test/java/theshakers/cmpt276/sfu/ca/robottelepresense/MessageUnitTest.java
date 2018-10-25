1705660d780064b30f6ad4346f1ed2d33346d5a7
subinb <subin_bae@sfu.ca>
2018-10-24 17:49:50 -0700

    Added Junit testing for Author, Message class

---
+++ Android/app/src/test/java/theshakers/cmpt276/sfu/ca/robottelepresense/MessageUnitTest.java
@@ -0,0 +1,23 @@
+package theshakers.cmpt276.sfu.ca.robottelepresense;
+
+import org.junit.Test;
+
+import theshakers.cmpt276.sfu.ca.robottelepresense.Model.Author;
+import theshakers.cmpt276.sfu.ca.robottelepresense.Model.Message;
+
+import static org.junit.Assert.assertEquals;
+
+public class MessageUnitTest {
+    @Test
+    public void creation_isCorrect() throws Exception {
+        Message messageWithDefaultConstructor = new Message();
+        assertEquals(messageWithDefaultConstructor.getId(), "message_id");
+        assertEquals(messageWithDefaultConstructor.getUser().getName(), "author_name");
+
+        Author test = new Author("test", "null", "pepper");
+        Message messageWithParmeterizedConstructor = new Message ("test", "null", test, null);
+        assertEquals(messageWithParmeterizedConstructor.getId(), "test");
+        assertEquals(messageWithParmeterizedConstructor.getText(), "null");
+        assertEquals(messageWithParmeterizedConstructor.getUser().getName(), "null");
+    }
+}
