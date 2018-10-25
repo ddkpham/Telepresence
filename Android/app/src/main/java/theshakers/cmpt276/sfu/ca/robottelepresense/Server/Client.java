97e7d98f42d5bc01899f954697d941fa611fbf50
subinb <subin_bae@sfu.ca>
2018-10-25 16:34:25 -0700

    Fixes getting string value and changed UI and comments - fixed error in getting string value in acitivity classes - changed some comments for class - edited some layouts to look better

--- Android/app/src/main/java/theshakers/cmpt276/sfu/ca/robottelepresense/Server/Client.java
+++ Android/app/src/main/java/theshakers/cmpt276/sfu/ca/robottelepresense/Server/Client.java
@@ -5,14 +5,13 @@ import android.util.Log;
 
 import org.json.JSONObject;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.Socket;
 import java.net.SocketException;
 
+import theshakers.cmpt276.sfu.ca.robottelepresense.BaseApplication;
 import theshakers.cmpt276.sfu.ca.robottelepresense.R;
 
 /**
@@ -41,6 +40,7 @@ public class Client extends AsyncTask <String, Void, String>{
             udpSocket = new DatagramSocket(serverPort);
             JSONObject jsonData = new JSONObject();
             jsonData.put("msg", params[0]);
+            Log.i(TAG, "sent message: " + params[0]);
             byte[] buf = jsonData.toString().getBytes();
 
             inetAddress = InetAddress.getByName("10.0.0.3");
@@ -57,18 +57,20 @@ public class Client extends AsyncTask <String, Void, String>{
             try {
                 JSONObject jsonObject = new JSONObject(datafromPacket);
                 returnMsg = jsonObject.getString("msg");
-                Log.i("Parsed to JSON: ", jsonObject.getString("msg"));
+                Log.i(TAG, "Parsed to JSON: " + jsonObject.getString("msg"));
             } catch (Throwable tx) {
-                Log.i("My App", "Could not parse malformed JSON: \"" + datafromPacket + "\"");
+                returnMsg = BaseApplication.getInstance().getResources().getString(R.string.error_wrong_attempt);
+                Log.i(TAG, "Could not parse malformed JSON: " + datafromPacket );
             }
 
         } catch (SocketException e) {
             Log.e(TAG, "SocketException, " + e);
-            returnMsg = Integer.toString(R.string.connectionError);
+            returnMsg = BaseApplication.getInstance().getResources().getString(R.string.error_connection);
         } catch (IOException e) {
             Log.e(TAG, "IOException, " + e);
         } catch (Exception e) {
             Log.e(TAG, "Exception, " + e);
+            returnMsg = BaseApplication.getInstance().getResources().getString(R.string.error_connection);
         } finally {
             udpSocket.close();
         }
@@ -78,7 +80,7 @@ public class Client extends AsyncTask <String, Void, String>{
     @Override
     protected void onPostExecute(String result) {
         if(result.equals("")) {
-            result = Integer.toString(R.string.clientResult1);
+            result = BaseApplication.getInstance().getResources().getString(R.string.error_wrong_attempt);
         }
         serverResponseCallback.onResponseReceived(result);
     }
