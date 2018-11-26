62a8c458133cef1a82d70baff41722a2fc90dffe
subinb <subin_bae@sfu.ca>
2018-11-26 14:36:58 -0800

    Added Game Result Activity for Hangman Game on Android

--- Android/app/src/main/java/theshakers/cmpt276/sfu/ca/robottelepresense/App.java
+++ Android/app/src/main/java/theshakers/cmpt276/sfu/ca/robottelepresense/App.java
@@ -7,7 +7,7 @@ package theshakers.cmpt276.sfu.ca.robottelepresense;
 // This class is for managing common variables such as http address used in http connection
 public class App {
     public static String httpAddress = "http://python-server-221001.appspot.com/";
-    //public static String httpAddress = "http://10.0.0.5:8080/";
+    //public static String httpAddress = "http://10.0.0.9:8080/";
     //httpAddress used for testing in localhost
     //public static String httpAddress = "http://207.23.196.16:8080/";
 }
